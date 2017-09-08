f6844e63a5b5704557c9131eabe54e5c609965e9
Frederik Foss Nielsen <frederik2812@gmail.com>
2017-09-08 14:30:02 +0200

    update

--- DOMBasics/FirstDocument/Script.js
+++ DOMBasics/FirstDocument/Script.js
@@ -1,13 +1,66 @@
 var h1 = document.querySelector("h1");
 var body = document.querySelector("body");
-var isBlue = false;
-setInterval(function(){
-  if (isBlue)  {
-    body.style.background = "white";
-    h1.style.color = "white";
-  } else {
-    body.style.background = "black";
-    h1.style.color = "black";
+var fps = prompt("How many Fps you want to show on screen?");
+var first = 255;
+var second = 255;
+var third = 255;
+var final = "rgb(" + first+", "+ second+ ", " + third + ")"
+var red = [];
+var green = [];
+var blue = [];
+h1.style.color = final;
+var counter = 255;
+
+for (var i = 0; i <= 1530; i++) {
+  if (i <= 255 ) {
+    red.push(255);
+    green.push(i);
+    blue.push(0);
   }
-  isBlue = !isBlue;
-}, 1000/60);
+  else if (i <= 510){
+
+    red.push(counter);
+    counter --;
+    green.push(255);
+    blue.push(0);
+  }
+  else if (i <= 765) {
+    red.push(0);
+    green.push(255);
+    counter ++;
+    blue.push(counter);
+  }
+  else if (i <= 1020) {
+    red.push(0);
+    counter --;
+    green.push(counter);
+    blue.push(255);
+  }
+  else if (i <= 1275) {
+    counter ++;
+    red.push(counter);
+    green.push(0);
+    blue.push(255);
+  }
+  else {
+    counter --;
+    red.push(255);
+    green.push(0);
+    blue.push(counter);
+  }
+  console.log(counter);
+}
+
+var count = 0;
+setInterval(function(){
+  first = red[count];
+  second = green[count];
+  third = blue[count];
+  var final = "rgb(" + first+", "+ second+ ", " + third + ")"
+  h1.style.color = final;
+  body.style.background = final;
+  count ++;
+  if(count === 1531){
+    count = 0;
+  }
+}, 1000/fps);
